c813a80d89eb64e7b0d0d39588808bad73f9ede9
AaravSharma107 <2008539@apps.nsd.org>
2024-10-08 01:31:49 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,60 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App
+{
+    public static void main(String[] args) 
+    {
+        //PART 1
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int x = 1; x<= i; x++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
 
-package org.nhs.app;
+        System.out.println();
 
-// Your code goes here
+        //PART 2
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int x = 4; x>= i; x--)
+            {
+                System.out.print(".");
+            }
+            for (int a = 1; a <= i; a++)
+            {   
+                System.out.print(i);
+            }
+            System.out.println();   
+        }
+
+        System.out.println();
+        
+        //PART 3
+        for (int i = 1; i < 6; i++)
+        {
+            for (int x = 4; x>= i; x--)
+            {
+                System.out.print(".");
+            }
+            
+            System.out.print(i);
+
+            for (int c = 1; c< i; c++)
+            {
+                System.out.print(".");
+            }
+
+            System.out.println();
+        }
+
+        /*for (int i = 1; i <= 5; i++)
+        {
+            for (int c = 1; c <= 4; c++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }*/
+    }
+}
